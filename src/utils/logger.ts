324a09dac845b38fe7398dc3aeb2b486709186dc
LedezmaSune <1yaminus@gmail.com>
2026-05-31 23:20:14 -0600

    fix: pino-pretty transport resolution error in Termux

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,12 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
+
+const stream = pretty({
+    colorize: true,
+    translateTime: 'SYS:standard',
+    ignore: 'pid,hostname',
+});
 
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname',
-        },
-    },
-});
+}, stream);
