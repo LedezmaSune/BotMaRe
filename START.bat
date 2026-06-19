fffaf0d9b3d8b77a3bc303bb9a27c63b1092e91d
LedezmaSune <1yaminus@gmail.com>
2026-06-19 00:10:44 -0600

    Feat: Fuerza la subida del atajo START.bat ignorado por gitignore

---
+++ START.bat
@@ -0,0 +1,11 @@
+@echo off
+title BotMaRe - Launcher
+color 0B
+echo =======================================================
+echo         INICIANDO CONTROL MAESTRO DE BOTMARE
+echo =======================================================
+echo.
+echo Ejecutando pnpm run menu...
+echo.
+pnpm run menu
+pause
